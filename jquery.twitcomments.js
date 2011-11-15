80835d7dd6aa7fd72bee722c5e856bedfe6157c8
Georg Dresler <georg@g-dresler.de>
2011-11-15 19:17:54 +0100

    Working quite ok

--- jquery.twitcomments.js
+++ jquery.twitcomments.js
@@ -10,10 +10,23 @@
     intents: false, // true to enable web intents, false otherwise
     defaultProfileImage: null,
     strings: {
-      name: 'Twitter Username',      // default value of name input field
-      comment: 'Write a comment...', // default value of comment textarea
-      submit: 'Comment',             // default value of submit button
-      location: 'from'               // user xyz from abc
+      twitterUsername: 'Twitter Username',
+      writeAComment: 'Write a comment...',
+      submit: 'Comment',
+      from: 'from',
+      justNow: 'just now',
+      aMinuteAgo: 'a minute ago',
+      minutesAgo: 'minutes ago',
+      oneHourAgo: 'one hour ago',
+      hoursAgo: 'hours ago',
+      yesterday: 'yesterday',
+      daysAgo: 'days ago',
+      aWeekAgo: 'a week ago',
+      weeksAgo: 'weeks ago',
+      aMonthAgo: 'a month ago',
+      monthsAgo: 'months ago',
+      aYearAgo: 'a year ago',
+      yearsAgo: 'years ago'
     }
   };
   
@@ -30,10 +43,6 @@
       methods._setupEvents.apply(this);
       methods._pullComments.apply(this);
       
-      // display form
-      
-      // display comments
-      
       return this;
     },
     
@@ -47,8 +56,8 @@
         
         form = $('<form class="'+classPrefix+'form" action="'+action+'" method="post"></form>');
         form.append($('<span class="'+classPrefix+'span '+classPrefix+'at-prefix">@</span>'));
-        form.append($('<input type="text" name="'+classPrefix+'name" class="'+classPrefix+'input '+classPrefix+'input-name" value="'+defaults.strings.name+'" />'));
-        form.append($('<textarea name="'+classPrefix+'comment" class="'+classPrefix+'textarea '+classPrefix+'input-comment">'+defaults.strings.comment+'</textarea>'));
+        form.append($('<input type="text" name="'+classPrefix+'screen_name" class="'+classPrefix+'input '+classPrefix+'input-name" value="'+defaults.strings.twitterUsername+'" />'));
+        form.append($('<textarea name="'+classPrefix+'comment" class="'+classPrefix+'textarea '+classPrefix+'input-comment">'+defaults.strings.writeAComment+'</textarea>'));
         form.append($('<input type="submit" name="'+classPrefix+'submit" class="'+classPrefix+'button '+classPrefix+'input-submit" value="'+defaults.strings.submit+'" />'));
         
         this.append(form);
@@ -66,25 +75,28 @@
     
     _setupEvents: function() {
       
-      $('.'+classPrefix+'input-name, .'+classPrefix+'input-comment').on('focus.twitcomments', function() {
+      $('.'+classPrefix+'input-name, .'+classPrefix+'input-comment').on('focus.twitcomments', function () {
         $(this).data('originalValue', $(this).val());
         $(this).val('');
       });
       
-      $('.'+classPrefix+'input-name, .'+classPrefix+'input-comment').on('blur.twitcomments', function() {
+      $('.'+classPrefix+'input-name, .'+classPrefix+'input-comment').on('blur.twitcomments', function () {
         originalValue = $(this).data('originalValue');
         if ($(this).val() == '') {
           $(this).val(originalValue);
         }
       });
       
-      $('.'+classPrefix+'input-name').on('blur.twitcomments', function() {
+      $('.'+classPrefix+'input-name').on('blur.twitcomments', function () {
         methods._updateUserInfo($(this).val());
       });
       
-      $('.'+classPrefix+'form').on('submit.twitcomments', function() {
-        methods._submit();
-      });
+      if (defaults.pushURL) {
+        $('.'+classPrefix+'form').on('submit.twitcomments', function (e) {
+          methods._submit();
+          e.preventDefault();
+        });
+      }
       
       return this;
     },
@@ -102,10 +114,11 @@
         data: { 'screen_name': screenName },
         dataType: 'jsonp',
         success: function(data) {
+          $this.data('userExists', true);
           infoBlock.html(methods._getNewUserBlock(data)).fadeIn();
         },
         error: function(jqXHR, textStatus, errorThrown) {
-          infoBlock.html('').fadeIn();
+          // TODO
         }
       });
       
@@ -136,6 +149,31 @@
       return this;
     },
     
+    _submit: function() {
+      if ($('.'+classPrefix+'form .'+classPrefix+'input-name').val() == '') {
+        // TODO
+        alert('User empty');
+      } else if (!$this.data('userExists')) {
+        // TODO
+        alert('User no exist');
+      } else {
+        $.ajax({
+          url: defaults.pushURL,
+          method: 'POST',
+          data: { foo: 'bar' },
+          dataType: 'json',
+          success: function(data) {
+            infoBlock = methods._getNewUserBlock(data).hide();
+            $('.'+classPrefix+'comments').prepend(infoBlock);
+            infoBlock.fadeIn();
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+            // TODO
+          }
+        });
+      }
+    },
+    
     _getNewUserBlock: function(data) {
       
       imageURL = (data.profile_image_url) ? data.profile_image_url : defaults.defaultProfileImage;
@@ -144,20 +182,64 @@
       
       block = $('<div></div>');
       block.append($('<img src="'+imageURL+'" alt="" class="'+classPrefix+'user-image" />'));
-      block.append($('<span class="'+classPrefix+'span '+classPrefix+'user-fullname">'+fullName+'</span>'));
-      block.append($('<span class="'+classPrefix+'span '+classPrefix+'user-twittername"><a href="'+twitterURL+'">@'+data.screen_name+'</a></span>'));
-      
-      if (data.comment_timestamp) {
-        block.append($('<span class="'+classPrefix+'span '+classPrefix+'comment-time">'+data.comment_timestamp+'</span>'));
+      if (data.name) {
+        block.append($('<span class="'+classPrefix+'span '+classPrefix+'user-fullname">'+fullName+'</span>'));
       }
-      
-      block.append($('<span class="'+classPrefix+'span '+classPrefix+'user-location">'+defaults.strings.location+' '+data.location+'</span>'));
-      
+      block.append($('<span class="'+classPrefix+'span '+classPrefix+'user-twittername"><a href="'+twitterURL+'">@'+data.screen_name+'</a></span>'));
+      if (data.comment_timestamp) {
+        block.append($('<span class="'+classPrefix+'span '+classPrefix+'comment-time">'+methods._relativeDate(new Date(data.comment_timestamp), new Date())+'</span>'));
+      }
+      if (data.location) {
+        block.append($('<span class="'+classPrefix+'span '+classPrefix+'user-location">'+defaults.strings.from+' '+data.location+'</span>'));
+      }
       if (data.comment_content) {
         block.append($('<span class="'+classPrefix+'span '+classPrefix+'comment-content">'+data.comment_content+'</span>'));
       }
       
       return block;
+    },
+    
+    /**
+     * from https://github.com/azer/relative-date
+     */
+    _relativeDate: function(input, reference) {
+      var SECOND = 1000,
+        MINUTE = 60 * SECOND,
+        HOUR = 60 * MINUTE,
+        DAY = 24 * HOUR,
+        WEEK = 7 * DAY,
+        YEAR = DAY * 365,
+        MONTH = YEAR / 12;
+
+      var formats = [
+        [ 0.7 * MINUTE, defaults.strings.justNow ],
+        [ 1.5 * MINUTE, defaults.strings.aMinuteAgo ],
+        [ 60 * MINUTE, defaults.strings.minutesAgo, MINUTE ],
+        [ 1.5 * HOUR, defaults.strings.oneHourAgo ],
+        [ DAY, defaults.strings.hoursAgo, HOUR ],
+        [ 2 * DAY, defaults.strings.yesterday ],
+        [ 7 * DAY, defaults.strings.daysAgo, DAY ],
+        [ 1.5 * WEEK, defaults.strings.aWeekAgo],
+        [ MONTH, defaults.strings.weeksAgo, WEEK ],
+        [ 1.5 * MONTH, defaults.strings.aMonthAgo ],
+        [ YEAR, defaults.strings.monthsAgo, MONTH ],
+        [ 1.5 * YEAR, defaults.strings.aYearAgo ],
+        [ Number.MAX_VALUE, defaults.strings.yearsAgo, YEAR ]
+      ];
+
+      !reference && ( reference = (new Date).getTime() );
+      reference instanceof Date && ( reference = reference.getTime() );
+      input instanceof Date && ( input = input.getTime() );
+    
+      var delta = reference - input,
+          format, i, len;
+
+      for(i = -1, len=formats.length; ++i < len; ) {
+        format = formats[i];
+        if(delta < format[0]){
+          return format[2] == undefined ? format[1] : Math.round(delta/format[2]) + ' ' + format[1];
+        }
+      }
     }
   };
   
